713ae2db3418e31865d0c4cd0f1811ab37d56aad
Isabella Santacruz <isabella.santacruz@polymtl.ca>
2024-02-07 14:44:27 -0500

    Fonction TP3 2.1

--- TP3/part2.c
+++ TP3/part2.c
@@ -8,7 +8,7 @@
 // TODO
 // Si besoin, ajouter ici les directives d'inclusion
 // -------------------------------------------------
-
+#include <pthread.h> 
 // -------------------------------------------------
 
 #include "./libprocesslab/libprocesslab.h"
@@ -26,14 +26,43 @@ long somme[nb];
 // fonction exécutée par chaque thread créé
 void* contribution(void*p)
 {
-    // TODO
+  // TODO
+  int no = *((int*)p);
+  somme[no] = 0;
 
-  return NULL;
+  for(int i = no * m / nb + 1; i <= (no + 1) * m / nb; i++) {
+    somme[no] += i;
+  }
+
+  printf("somme pour thread %d: %ld\n", no, somme[no]);
+
+  pthread_exit(NULL);
 }
 
 
 void question2( )
 {
-    // TODO
-    
+  // TODO
+  pthread_t *tid = malloc (nb * sizeof(pthread_t));
+
+  int ordre[nb];
+  for(int i = 0; i < nb; i++) {
+    ordre[i] = i;
+  }
+  
+  for(int i = 0; i < nb; i++) {
+    pthread_create(&tid[i], NULL, contribution, &ordre[i]);
+  }
+
+  for(int i = 0; i < nb; i++) {
+    pthread_join(tid[i],NULL);
+  }
+
+  long sommeTotale = 0;
+
+  for(int i = 0; i < nb; i++) {
+    sommeTotale += somme[i];
+  }
+
+  printf("SommeTotale: %ld, Valeur attendue: %ld\n", sommeTotale, m * (m + (long)1) / 2);
 }
