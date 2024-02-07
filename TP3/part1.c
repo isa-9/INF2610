bbb897eee1b7bdc2662d643ef950bf784cb3ebee
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2024-02-07 13:08:44 -0500

    Début TP3

---
+++ TP3/part1.c
@@ -0,0 +1,20 @@
+/*
+ * processlab - part1.c
+ *
+ * Ecole polytechnique de Montreal, GIGL, Hiver  2024
+ * vos noms, prénoms et matricules
+*/
+
+#include "libprocesslab/libprocesslab.h"
+
+// TODO
+// Si besoin, ajouter ici les directives d'inclusion
+// -------------------------------------------------
+
+// -------------------------------------------------
+
+void question1()
+{
+   
+    // TODO
+}
---
+++ TP3/part2.c
@@ -0,0 +1,39 @@
+/*
+ * processlab - part2.c
+ *
+ * Ecole polytechnique de Montreal, GIGL, Hiver  2024
+ * vos noms, prénoms et matricules
+*/
+
+// TODO
+// Si besoin, ajouter ici les directives d'inclusion
+// -------------------------------------------------
+
+// -------------------------------------------------
+
+#include "./libprocesslab/libprocesslab.h"
+
+#define m 1000000
+// question2 calcule la somme des m premiers nombres naturels 1+2+...+m
+
+// nb est le nombre de threads qui vont contribuer au calcul
+#define nb 4
+
+//tableau somme est utilisé pour le calcul des sommes patielles par les threads
+long somme[nb];
+
+
+// fonction exécutée par chaque thread créé
+void* contribution(void*p)
+{
+    // TODO
+
+  return NULL;
+}
+
+
+void question2( )
+{
+    // TODO
+    
+}
