07911659f112ee93a9bcbeed6d18889df2359c22
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2024-03-13 14:03:28 -0400

    TP5 déclarations

--- TP5/GuerreDesChiffres.c
+++ TP5/GuerreDesChiffres.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 //..
+int totalProduit = 0;
+int totalConsomme = 0;
+int* tampon;
+// Semaphores
+//  -> Accès au
+bool flag_de_fin = false;
 
 void* producteur(void* pid) {
     // ...
@@ -17,6 +23,10 @@ int main(int argc, char* argv[]) {
     // Les paramètres du programme sont, dans l'ordre :
     // le nombre de producteurs, le nombre de consommateurs
     // et la taille du tampon.
+
+    int np = atoi(argv[1]);
+    int nc = atoi(argv[2]);
+    int tailleTampon = atoi(argv[3]);
 
     // ..
     return 0;
