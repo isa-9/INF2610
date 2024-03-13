896f63fd7fdb89b20936ac7013c36a138b8524e9
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2024-03-13 13:17:20 -0400

    debut tp5

---
+++ TP5/GuerreDesChiffres.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//..
+
+void* producteur(void* pid) {
+    // ...
+    return NULL;
+}
+
+void* consommateur(void* cid) {
+    // ...
+
+    return NULL;
+}
+// ...
+
+int main(int argc, char* argv[]) {
+    // Les paramètres du programme sont, dans l'ordre :
+    // le nombre de producteurs, le nombre de consommateurs
+    // et la taille du tampon.
+
+    // ..
+    return 0;
+}
